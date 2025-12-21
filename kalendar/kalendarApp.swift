1f2455902d71590b476c66f3a056af386de3cd04
itang1 <irenetang14@gmail.com>
2025-12-20 17:32:26 -0700

    Initial Commit

---
+++ kalendar/kalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  kalendarApp.swift
+//  kalendar
+//
+//  Created by Irene Tang on 12/20/25.
+//
+
+import SwiftUI
+
+@main
+struct kalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
